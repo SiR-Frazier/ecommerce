3ffe33dcc846d57a4b37e55a07cf2b3a8e93b25d
Alex Ficklin and Bryce Frazier <alex@>
2018-04-30 16:50:10 -0700

    final commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,8 +3,8 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     @order_item = current_order.order_items.new
-    book_object = Book.new("The Fallen")
-    @title = book_object.get_title()
+    # book_object = Book.new("The Fallen")
+    # @title = book_object.get_title()
   end
 
   def product_params
