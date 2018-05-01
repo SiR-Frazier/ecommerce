88479e2c6e115d6423efa1df99fa18ef977c4300
Alex Ficklin and Bryce Frazier <bryce@>
2018-05-01 14:47:15 -0700

    add style

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,7 +1,6 @@
 class ChargesController < ApplicationController
 
   def new
-    @charges = Charges.new
   end
 
 
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,30 @@ class ProductsController < ApplicationController
     # @title = book_object.get_title()
   end
 
+  def show
+    @product = Product.find(params[:id])
+    render :show
+  end
+
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { render :new }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def product_params
     params.require(:product).permit(:name, :price)
   end
