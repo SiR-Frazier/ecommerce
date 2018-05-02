75bae11ee60a5b7fc3e583275b4105f42d4d1531
Alex Ficklin and Bryce Frazier <alex@>
2018-05-02 10:19:39 -0700

    add styling and ajax functionality to website.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,6 +17,27 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+   @product = Product.find(params[:id])
+   if @product.update(product_params)
+     flash[:notice] = "updated product"
+     redirect_to products_path
+   else
+     flash[:alert] = "try again error occured"
+     render :edit
+   end
+ end
+
+ def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
   def create
     @product = Product.new(product_params)
 
